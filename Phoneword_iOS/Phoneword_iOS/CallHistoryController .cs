04d7df27e5c56561926227c76ed74e4d20b7d01a
Stéphano TELOLAHY <stephanotelolahy@gmail.com>
2016-11-17 16:34:25 +0100

    [feature] Hello, iOS

---
+++ Phoneword_iOS/Phoneword_iOS/CallHistoryController .cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Phoneword_iOS
+{
+    public partial class CallHistoryController  : UITableView
+    {
+        public CallHistoryController  (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
